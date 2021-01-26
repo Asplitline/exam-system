12bf563a1ed42349c55990fd3580eb75536ceb70
myqz <ambition292@163.com>
2021-01-26 15:05:54 +0800

    add login

---
+++ src/components/Login.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <el-form
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+        ref="loginFormRef"
+      >
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user-solid"
+            v-model="loginForm.username"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="el-icon-lock"
+            v-model="loginForm.password"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="info" @click="resetForm('loginFormRef')"
+            >重置</el-button
+          >
+          <el-button
+            type="success"
+            @click="submitForm('loginFormRef')"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' },
+          { min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 表单验证
+    submitForm(formName) {
+      this.$refs[formName].validate(async (valid) => {
+        if (valid) {
+          const params = this.toUrlParams()
+          const result = await this.$http.post('account/api/login', params)
+          const { message, success, data } = result.data
+          if (success === false) {
+            this.$message.error(message)
+          } else {
+            this.$message.success('登录成功')
+            window.sessionStorage.setItem('token', data.id)
+            this.$router.push('/home')
+          }
+        } else {
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    },
+    toUrlParams() {
+      let params = []
+      const form = this.loginForm
+      for (const key in form) {
+        params.push(key + '=' + form[key])
+      }
+      params = params.join('&')
+      return params
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #1e1e1e;
+}
+
+.login_box {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 0 10px #ddd;
+  .avatar_box {
+    width: 100px;
+    height: 100px;
+    border: 1px solid #ccc;
+    border-radius: 50%;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 40px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'
+import './plugins/element.js'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// 配置axios
+// 配置基础路径
+axios.defaults.baseURL = 'http://127.0.0.1:8088/'
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
